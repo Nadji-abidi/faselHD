99b5fe340ef0d9294a7d9058fa395917a3db5f48
Nadji-abidi <53299787+Nadji-abidi@users.noreply.github.com>
2022-05-22 15:07:02 +0100

    Add vsdownload

--- faselHD.py
+++ faselHD.py
@@ -1,13 +1,20 @@
 #! python
 import requests
-import os
+import os, sys
 import re
 from bs4 import BeautifulSoup as bs
-from youtube_dl.utils import random_user_agent
+import random
+from vsdownload import vsdownload
 
 
 soup = lambda url : bs(requests.get(url).content,"html.parser")
-dir_path = os.path.dirname(os.path.realpath(__file__))
+
+def random_user_agent(file=os.path.join(os.path.dirname(__file__),'user_agents.txt')):
+	with open(file, "r") as f:
+		lines = f.readlines()
+		user_agent = random.choice(lines).replace("\n", "")
+		f.close()
+	return str(user_agent)
 
 def search(user_input):
     search_URL = "https://www.faselhd.pro/?s=" + user_input.strip()
@@ -39,7 +46,7 @@ def select_episodes(media_url):
         try : episodes_num = [int("".join(re.findall(pattern,i))) for i in episodes]
         except ValueError : episodes_num = list(range(1,len(episodes) + 1))
         print(f"Episodes : {episodes_num[0]}-{episodes_num[-1]}")
-        start,end = [ input(f"\n {i} EP : ") for i in ["Start on","End in"] ]
+        start,end = [ input(f"\n {i} EP : ") for i in ("Start on","End in") ]
         sel = lambda i,opt : opt if i == "" else episodes_num.index(int(i))
         return episodes[sel(start,0):sel(end,len(episodes))]
 
@@ -47,33 +54,16 @@ def getDirectLink(site) :
     headers = {'User-Agent': f'{random_user_agent()}'}
     return requests.get(site,headers=headers).text
     
-def CQuality(available,prefer) :
-    all_qual = ["360","480","720","1080"]
-    if prefer in all_qual : 
-        return prefer if prefer in available else CQuality(available,all_qual[all_qual.index(prefer) - 1])
-    else :
-        return prefer
-
-def download(links,folder,normal=True):
-    if not(normal) : quality = input("Choose Your Preferde Resolution : [1080,720,480,360] ? ")
+def download(links, folder):
+    os.makedirs(folder)
     for link in links:
         link = soup(link)
         title = link.select_one(".h3").text
         title = " ".join(re.findall(r"\d+|\w+",title)) + ".mp4"
         print(f"\n\x1b[41m\x1b[37mDownloading \x1b[0m ===> {title}")
-        if normal :
-            downloadLink = link.select_one(".downloadLinks").a["href"]
-            down_page = soup(downloadLink)
-            url = down_page.select_one(".dl-link").a["href"]
-            os.system(f"wget '{url}' -O '{title}' --tries 10 -c --user-agent='{random_user_agent()}'")
-        else : 
-            link = getDirectLink(link.find("iframe")["src"])
-            videoURL = re.findall("\"file\":\"(.*)\",\"hlshtml\"",link)[0].replace("\\","")
-            available = re.findall(r",*(\d+)+,p*",videoURL)
-            if available : newVideoURL = re.sub(r",(.*),",f",{CQuality(available,quality)}",videoURL)
-            try : os.mkdir(folder)
-            except FileExistsError : os.chdir(folder)
-            os.system(f"downloadm3u8 -o '{folder}/{title}' '{newVideoURL or videoURL}'")
+        link = getDirectLink(link.find("iframe")["src"])
+        videoURL = re.findall("\"file\":\"(.*\.m3u8)\"",link)[0].replace("\\","")
+        vsdownload.save(videoURL, output=f"{folder}/{title}", max_quality=True)
 
 def main() :
     main_page = search(input("Search : "))
@@ -82,7 +72,9 @@ def main() :
         main_page = search(input("Search : "))
     selected = display_results(main_page)
     episodes = select_episodes(main_page[selected])
-    download(episodes,dir_path+selected,normal=False)
+    download(episodes, os.path.join(os.getcwd(),selected))
 
 try : main()
-except KeyboardInterrupt : print("\n\nExit .....")
+except KeyboardInterrupt : 
+    print("\n\nExit .....")
+    sys.exit()
